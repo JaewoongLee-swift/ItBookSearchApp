995d320d353ebd1d128afeba6f8ee71b3ec167da
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-08 03:14:28 +0900

    [Feat] #2 ItBookStore, ItBook 구조체 구현

--- ItBookSearchApp/ItBookSearchApp/ItBookStore.swift
+++ ItBookSearchApp/ItBookSearchApp/ItBookStore.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct ItBookStore {
+    let error: Int
+    let total: String
+    let page: String
+    let books: [ItBook]
+}
+
+struct ItBook {
+    let title: String
+    let subtitle: String
+    let isbn13: String
+    let price: String
+    let imageURL: String
+    let url: String
+}
