a9fdaeb16fbeb4fa0f03a537eb4477885b7df24c
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-08 03:29:02 +0900

    [Feat] #2 ItBookDetail, ItBookPDF 구조체 구현

--- ItBookSearchApp/ItBookSearchApp/ItBookStore.swift
+++ ItBookSearchApp/ItBookSearchApp/ItBookStore.swift
@@ -22,3 +22,27 @@ struct ItBook {
     let imageURL: String
     let url: String
 }
+
+struct ItBookDetail {
+    let error: Int
+    let title: String
+    let subtitle: String
+    let authors: String
+    let publisher: String
+    let language: String
+    let isbn10: String
+    let isbn13: String
+    let pages: String
+    let year: String
+    let rating: String
+    let desc: String
+    let price: String
+    let imageURL: String
+    let url: String
+    let pdf: ItBookPDF
+}
+
+struct ItBookPDF {
+    let chapter2: String
+    let chapter5: String
+}
