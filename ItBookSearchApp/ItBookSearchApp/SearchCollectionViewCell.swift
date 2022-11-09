1a047d0d1681549760531c22a5ef3c321cd52a7c
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 00:47:37 +0900

    [Layout] #5 SearchCollectionViewCell 내부 UI 컴포넌트 선언

--- ItBookSearchApp/ItBookSearchApp/SearchCollectionViewCell.swift
+++ ItBookSearchApp/ItBookSearchApp/SearchCollectionViewCell.swift
@@ -8,5 +8,39 @@
 import UIKit
 
 class SearchCollectionViewCell: UICollectionViewCell {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
     
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var isbn13Label: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var urlLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
 }
