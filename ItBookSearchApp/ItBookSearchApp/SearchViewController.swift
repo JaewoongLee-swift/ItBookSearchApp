8db8c8d108d71d7a1fde497f05ccc70a9849a03f
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 00:12:48 +0900

    [Layout] #5 SearchViewController 생성, ViewController 삭제

---
+++ ItBookSearchApp/ItBookSearchApp/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  ItBookSearchApp
+//
+//  Created by 이재웅 on 2022/11/10.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
