04e47797c461bc6ce95164d7b404a41d188b408d
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 00:50:27 +0900

    [Layout] #5 SearchCollectionViewCell 내부 UI 컴포넌트 설정

--- ItBookSearchApp/ItBookSearchApp/SearchCollectionViewCell.swift
+++ ItBookSearchApp/ItBookSearchApp/SearchCollectionViewCell.swift
@@ -10,36 +10,47 @@ import UIKit
 class SearchCollectionViewCell: UICollectionViewCell {
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
+        label.text = "title"
+        label.font = .systemFont(ofSize: 20.0, weight: .bold)
         
         return label
     }()
     
     private lazy var subtitleLabel: UILabel = {
         let label = UILabel()
+        label.text = "Subtitle"
+        label.font = .systemFont(ofSize: 16.0, weight: .medium)
         
         return label
     }()
     
     private lazy var isbn13Label: UILabel = {
         let label = UILabel()
+        label.text = "isbn13"
+        label.font = .systemFont(ofSize: 12.0)
         
         return label
     }()
     
     private lazy var priceLabel: UILabel = {
         let label = UILabel()
+        label.text = "$price"
+        label.font = .systemFont(ofSize: 12.0)
         
         return label
     }()
     
     private lazy var urlLabel: UILabel = {
         let label = UILabel()
+        label.text = "https://url"
+        label.font = .systemFont(ofSize: 12.0)
         
         return label
     }()
     
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.backgroundColor = .blue
         
         return imageView
     }()
