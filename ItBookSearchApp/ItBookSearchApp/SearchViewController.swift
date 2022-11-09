dc5b86782f8ff807f7ae14d033f4ca549bae0646
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 00:31:38 +0900

    [Layout] #5 tableView DataSoure, Delegate 프로토콜 채택

--- ItBookSearchApp/ItBookSearchApp/SearchViewController.swift
+++ ItBookSearchApp/ItBookSearchApp/SearchViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 class SearchViewController: UIViewController {
     lazy var tableView: UITableView = {
         let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
         
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //TODO: [ItBook]의 count를 return하도록 구현
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //TODO: Custom UITableViewCell 구현
+        UITableViewCell()
     }
 }
