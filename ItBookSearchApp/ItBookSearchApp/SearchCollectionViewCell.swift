c8a076e736cf4216e141b6de2c892502eb7c593c
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 00:43:05 +0900

    [Layout] #5 SearchCollectionViewCell 생성

---
+++ ItBookSearchApp/ItBookSearchApp/SearchCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchCollectionViewCell.swift
+//  ItBookSearchApp
+//
+//  Created by 이재웅 on 2022/11/10.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+}
