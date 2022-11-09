ba7e9fb9c21b8b285cffee7167194a43b52a6b5d
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 01:40:41 +0900

    [Layout] #5 SearchCollectionViewCell의 setupLayout 메소드 구현(Constraints Unit Test 필요)

--- ItBookSearchApp/ItBookSearchApp/SearchCollectionViewCell.swift
+++ ItBookSearchApp/ItBookSearchApp/SearchCollectionViewCell.swift
@@ -73,5 +73,30 @@ extension SearchCollectionViewCell {
             imageView
         ].forEach { addSubview($0) }
         
+        //TODO: Constraints Unit Test 필요
+        imageView.topAnchor.constraint(equalTo: topAnchor, constant: -20).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: -20).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 150.0).isActive = true
+        
+        titleLabel.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+        
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8.0).isActive = true
+        subtitleLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        subtitleLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
+        
+        isbn13Label.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 8.0).isActive = true
+        isbn13Label.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        isbn13Label.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
+        
+        priceLabel.topAnchor.constraint(equalTo: isbn13Label.bottomAnchor, constant: 8.0).isActive = true
+        priceLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        priceLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
+        
+        urlLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 8.0).isActive = true
+        urlLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        urlLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
     }
 }
