a4738029eac5cf67a15033cb3fb27be538b8f3a6
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 21:32:46 +0900

    [Layout] #7 DetailViewController 생성

---
+++ ItBookSearchApp/ItBookSearchApp/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  ItBookSearchApp
+//
+//  Created by 이재웅 on 2022/11/10.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
