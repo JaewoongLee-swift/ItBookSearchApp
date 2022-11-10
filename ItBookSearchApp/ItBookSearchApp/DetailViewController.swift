a177cfbfc52fcf3e81fcfd87b0974b2f1b84ee21
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 21:37:27 +0900

    [Layout] #7 DetailViewController 내부 UI 컴포넌트 선언

--- ItBookSearchApp/ItBookSearchApp/DetailViewController.swift
+++ ItBookSearchApp/ItBookSearchApp/DetailViewController.swift
@@ -8,6 +8,96 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var authorLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var isbn10Label: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var isbn13Label: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var pageLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var yearLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var ratingLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var urlLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var errorLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var firstPDFButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    
+    private lazy var secondPDFButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
