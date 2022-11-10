dbd2eb8c0fd46f12b72cf8957aca8b661fd127b7
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 11:07:42 +0900

    [Layout] #5SearchViewControllerTests 생성

---
+++ ItBookSearchApp/ItBookSearchAppTests/SearchViewControllerTests.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewControllerTests.swift
+//  ItBookSearchAppTests
+//
+//  Created by 이재웅 on 2022/11/10.
+//
+
+import XCTest
+@testable import ItBookSearchApp
+
+final class SearchViewControllerTests: XCTestCase {
+    
+    var searchViewController: SearchViewController?
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
