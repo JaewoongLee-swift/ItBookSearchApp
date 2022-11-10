59843b31fb8efc3b6968469069446c51968ff29a
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-10 11:24:47 +0900

    [Layout] #5 SearchViewController의 setupLayout 메소드 구현(collectionView Cell 등록 후 테스트)

--- ItBookSearchApp/ItBookSearchApp/SearchViewController.swift
+++ ItBookSearchApp/ItBookSearchApp/SearchViewController.swift
@@ -13,12 +13,16 @@ class SearchViewController: UIViewController {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = true
+        collectionView.backgroundColor = .red
         
         return collectionView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .blue
+        setupLayout()
     }
 }
 
@@ -35,5 +39,19 @@ extension SearchViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         //TODO: Custom UICollectionViewCell 구현
         UICollectionViewCell()
+    }
+}
+
+extension SearchViewController {
+    //TODO: collectionView의 셀이 모두 등록된 이후 테스트 필요
+    func setupLayout() {
+        view.addSubview(collectionView)
+        
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        collectionView.heightAnchor.constraint(equalToConstant: view.frame.height).isActive = true
     }
 }
--- ItBookSearchApp/ItBookSearchAppTests/SearchViewControllerTests.swift
+++ ItBookSearchApp/ItBookSearchAppTests/SearchViewControllerTests.swift
@@ -13,11 +13,30 @@ final class SearchViewControllerTests: XCTestCase {
     var searchViewController: SearchViewController?
 
     override func setUpWithError() throws {
-        
+        searchViewController = SearchViewController()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func test_searchCollectionView_setupLayout_subViewCount() {
+        searchViewController?.setupLayout()
+        
+        let result = searchViewController?.view.subviews.count
+        
+        XCTAssertEqual(1, result, "SearchViewController의 view에 collectionView가 제대로 추가되지 않았습니다.")
+    }
+    
+    //TODO: Cell이 모두 구현된 후 테스트 필요
+//    func test_searchCollectionView_setupLayout_constraints() {
+//
+//        let result = (searchViewController?.collectionView.frame.width, searchViewController?.collectionView.frame.height)
+//        print(result)
+//        let view = (searchViewController?.view.frame.width, searchViewController?.view.frame.height)
+//        print(view)
+//
+//        XCTAssertTrue(result == view, "SearchViewController의 collectionView의 constraints가 제대로 설정되지 않았습니다.")
+//    }
 
 }
