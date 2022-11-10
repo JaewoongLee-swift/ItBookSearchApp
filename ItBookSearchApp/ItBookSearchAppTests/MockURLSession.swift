267a3d00ce45277c34ee54c35e75c0365b63f496
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-11 02:32:45 +0900

    [Feat] #9 MockURLSession 구현

--- ItBookSearchApp/ItBookSearchAppTests/MockURLSession.swift
+++ ItBookSearchApp/ItBookSearchAppTests/MockURLSession.swift
@@ -9,7 +9,19 @@ import Foundation
 @testable import ItBookSearchApp
 
 class MockURLSession: URLSessionProtocol {
+    typealias Response = (data: Data?, urlResponse: URLResponse?, error: Error?)
+    
+    let response: Response
+    
+    init(response: Response) {
+        self.response = response
+    }
+    
     func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        
+        return MockURLSessionDataTask {
+            completionHandler(self.response.data,
+                              self.response.urlResponse,
+                              self.response.error)
+        }
     }
 }
