c7ca32f46d42cbe2193baa4abc37086a6a697db0
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-11 02:27:53 +0900

    [Feat] #9 MockURLSessionDataTask 생성

---
+++ ItBookSearchApp/ItBookSearchAppTests/MockURLSessionDataTask.swift
@@ -0,0 +1,22 @@
+//
+//  MockURLSessionDataTask.swift
+//  ItBookSearchAppTests
+//
+//  Created by 이재웅 on 2022/11/11.
+//
+
+import Foundation
+@testable import ItBookSearchApp
+
+class MockURLSessionDataTask: URLSessionDataTask {
+    private let resumeHandler: () -> Void
+    
+    init(resumeHandler: @escaping () -> Void) {
+        self.resumeHandler = resumeHandler
+    }
+    
+    //실제 네트워크 요청이 아닌 단순 completionHandler 호출용
+    override func resume() {
+        resumeHandler()
+    }
+}
