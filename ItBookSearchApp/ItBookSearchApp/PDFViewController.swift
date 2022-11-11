970ab9988df177f6c0e28bf55498092692023dc5
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-12 05:45:41 +0900

    [Feat] #17 PDFViewController에 PDFKit import 및 pdfView 선언, 설정

--- ItBookSearchApp/ItBookSearchApp/PDFViewController.swift
+++ ItBookSearchApp/ItBookSearchApp/PDFViewController.swift
@@ -6,8 +6,16 @@
 //
 
 import UIKit
+import PDFKit
 
 class PDFViewController: UIViewController {
+    private lazy var pdfView: PDFView = {
+        let pdfView = PDFView()
+        pdfView.autoScales = true
+        pdfView.displayMode = .singlePageContinuous
+        
+        return pdfView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
