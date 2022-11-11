66fd277734300836ee3135a8887505c12a9f1c38
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-12 05:47:01 +0900

    [Feat] #17 PDFViewController가 url을 전달받도록 init 구현 및 변수선언

--- ItBookSearchApp/ItBookSearchApp/PDFViewController.swift
+++ ItBookSearchApp/ItBookSearchApp/PDFViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import PDFKit
 
 class PDFViewController: UIViewController {
+    var url: String?
+    
     private lazy var pdfView: PDFView = {
         let pdfView = PDFView()
         pdfView.autoScales = true
@@ -16,6 +18,16 @@ class PDFViewController: UIViewController {
         
         return pdfView
     }()
+    
+    init(url: String) {
+        self.url = url
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
