34c6d525027dc75d66b22fd5b13c660f3cd27cd9
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-11 23:41:16 +0900

    [Feat] #9 requestItBookStore 메소드 구현(Unit Test 완료)

--- ItBookSearchApp/ItBookSearchApp/SearchViewController.swift
+++ ItBookSearchApp/ItBookSearchApp/SearchViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    var itBookStoreManager: ItBookStoreManager?
     var itBookStore: ItBookStore?
     
     var books: [ItBook]? {
@@ -136,5 +137,21 @@ extension SearchViewController {
         searchController.searchBar.delegate = self
         
         navigationItem.searchController = searchController
+    }
+    
+    func requestItBookStore(from title: String, by manager: ItBookStoreManager = ItBookStoreManager()) {
+        itBookStoreManager = manager
+        
+        itBookStoreManager?.requestItBookStore(bookName: title) { [weak self] response in
+            print("실행은 되냐?")
+            if case .success(let data) = response {
+                print("성공")
+                self?.itBookStore = data
+                print(self?.itBookStore)
+            } else if case .failure(let error) = response {
+                print("에러")
+                print(error)
+            }
+        }
     }
 }
