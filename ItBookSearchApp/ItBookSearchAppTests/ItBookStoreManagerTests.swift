f84bcf2e4158c394a9ea2c8c5d6db51241b44936
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-11 19:34:43 +0900

    [Feat] #9 ItBookStore Unit Test 코드 구현

--- ItBookSearchApp/ItBookSearchAppTests/ItBookStoreManagerTests.swift
+++ ItBookSearchApp/ItBookSearchAppTests/ItBookStoreManagerTests.swift
@@ -24,9 +24,10 @@ final class ItBookStoreManagerTests: XCTestCase {
     
     func test_fetchData_statusCode_is_200() {
         //given
-        let url = "https://api.itbook.store/1.0/search/mongodb"
+        let searchText = "mongoDB"
+        let url = "https://api.itbook.store/1.0/search/\(searchText)"
         let mockResponse: MockURLSession.Response = {
-            let data: Data? = JsonLoader.data(fileName: "ItBookStore")
+            let data: Data? = JsonLoader.data(fileName: "MongoDBItBookStore")
             let successResponse = HTTPURLResponse(
                 url: URL(string: url)!,
                 statusCode: 200,
@@ -42,15 +43,119 @@ final class ItBookStoreManagerTests: XCTestCase {
         
         //when
         var result: ItBookStore?
-        sut.requestItBookStore(bookName: "mongodb") { response in
+        sut.requestItBookStore(bookName: searchText) { response in
             if case let .success(itBookStore) = response {
                 result = itBookStore
             }
         }
         
         //then
-        let expectation: ItBookStore? = JsonLoader.load(type: ItBookStore.self, fileName: "ItBookStore")
+        let expectation: ItBookStore? = JsonLoader.load(type: ItBookStore.self, fileName: "MongoDBItBookStore")
         XCTAssertEqual(result?.total, expectation?.total)
-        XCTAssertEqual(result?.books.first?.title, expectation?.books.first?.title)
+        XCTAssertEqual(result?.error, expectation?.error)
+        XCTAssertEqual(result?.page, expectation?.page)
+        XCTAssertEqual(result?.books.count, expectation?.books.count)
+    }
+    
+    func test_fetchData_searchResult_is_empty() {
+        //given
+        let searchText = "fefagawgrgrga"
+        let url = "https://api.itbook.store/1.0/search/\(searchText)"
+        let mockResponse: MockURLSession.Response = {
+            let data: Data? = JsonLoader.data(fileName: "EmptyItBookStore")
+            let successResponse = HTTPURLResponse(
+                url: URL(string: url)!,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            
+            return (data: data, urlResponse: successResponse, error: nil)
+        }()
+        
+        let mockURLSession = MockURLSession(response: mockResponse)
+        let sut = ItBookStoreManager(session: mockURLSession)
+        
+        //when
+        var result: ItBookStore?
+        sut.requestItBookStore(bookName: searchText) { response in
+            if case let .success(itBookStore) = response {
+                result = itBookStore
+            }
+        }
+        
+        //then
+        let expectation: ItBookStore? = JsonLoader.load(type: ItBookStore.self, fileName: "EmptyItBookStore")
+        XCTAssertEqual(result?.books.count, expectation?.books.count)
+        XCTAssertEqual(result?.total, expectation?.total)
+        XCTAssertEqual(result?.error, expectation?.error)
+        XCTAssertEqual(result?.page, expectation?.page)
+    }
+    
+    func test_fetchData_searchText_is_empty() {
+        //given
+        let searchText = ""
+        let url = "https://api.itbook.store/1.0/search/\(searchText)"
+        let mockResponse: MockURLSession.Response = {
+            let data: Data? = JsonLoader.data(fileName: "SearchTextNilItBookStore")
+            let successResponse = HTTPURLResponse(
+                url: URL(string: url)!,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            
+            return (data: data, urlResponse: successResponse, error: nil)
+        }()
+        
+        let mockURLSession = MockURLSession(response: mockResponse)
+        let sut = ItBookStoreManager(session: mockURLSession)
+        
+        //when
+        var result: ItBookStore?
+        sut.requestItBookStore(bookName: searchText) { response in
+            if case let .success(itBookStore) = response {
+                result = itBookStore
+            }
+        }
+        
+        //then
+        let expectation: ItBookStore? = JsonLoader.load(type: ItBookStore.self, fileName: "SearchTextNilItBookStore")
+        XCTAssertEqual(result?.books.count, expectation?.books.count)
+        XCTAssertEqual(result?.total, expectation?.total)
+        XCTAssertEqual(result?.error, expectation?.error)
+        XCTAssertEqual(result?.page, expectation?.page)
+    }
+    
+    func test_fetchData_statusCode_is_500() {
+        //given
+        let searchText = "mongoDB"
+        let url = "https://api.itbook.store/1.0/search/\(searchText)"
+        let mockResponse: MockURLSession.Response = {
+            let data: Data? = JsonLoader.data(fileName: "MongoDBItBookStore")
+            let successResponse = HTTPURLResponse(
+                url: URL(string: url)!,
+                statusCode: 500,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            
+            return (data: data, urlResponse: successResponse, error: nil)
+        }()
+        
+        let mockURLSession = MockURLSession(response: mockResponse)
+        let sut = ItBookStoreManager(session: mockURLSession)
+        
+        //when
+        var result: ItBookStoreError?
+        sut.requestItBookStore(bookName: searchText) { response in
+            if case let .failure(error) = response {
+                result = error as? ItBookStoreError
+            }
+        }
+        
+        //then
+        let expectation: ItBookStoreError = ItBookStoreError.requestFailError
+        XCTAssertEqual(result, expectation)
     }
 }
