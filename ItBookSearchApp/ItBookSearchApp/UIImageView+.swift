83fec8bd804e39fd2888cc06ebbae074cc7dce35
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-12 17:14:04 +0900

    [Feat] #19 UIImageView extension에 이미지 캐시를 지원하는 setImage 메소드 구현(Unit Test 필요

--- ItBookSearchApp/ItBookSearchApp/UIImageView+.swift
+++ ItBookSearchApp/ItBookSearchApp/UIImageView+.swift
@@ -8,5 +8,26 @@
 import UIKit
 
 extension UIImageView {
-    
+    //TODO: Unit Test 필요
+    func setImage(url: String) {
+        DispatchQueue.global(qos: .background).async {
+            let cachedKey = NSString(string: "url")
+            
+            if let cachedImage = ImageCacheManager.shared.object(forKey: cachedKey) {
+                self.image = cachedImage
+                
+                return
+            }
+            
+            guard let url = URL(string: url) else { return }
+            guard let data = try? Data(contentsOf: url) else { return }
+            
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data) {
+                    ImageCacheManager.shared.setObject(image, forKey: cachedKey)
+                    self.image = image
+                }
+            }
+        }
+    }
 }
