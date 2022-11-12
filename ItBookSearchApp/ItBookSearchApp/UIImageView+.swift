ab350cba752fb2e2f8f5901201f8f7d27fa74ccd
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-12 19:41:59 +0900

    [Feat] #19 FileManager를 이용한 setImage 메소드에 이미지 디스크캐시 구현

--- ItBookSearchApp/ItBookSearchApp/UIImageView+.swift
+++ ItBookSearchApp/ItBookSearchApp/UIImageView+.swift
@@ -11,14 +11,36 @@ extension UIImageView {
     //TODO: Unit Test 필요
     func setImage(url: String) {
         DispatchQueue.global(qos: .background).async {
-            let cachedKey = NSString(string: url)
+            let cachedKey = NSString(string: url).lastPathComponent
             
-            if let cachedImage = ImageCacheManager.shared.object(forKey: cachedKey) {
+            if let cachedImage = ImageCacheManager.shared.object(forKey: cachedKey as NSString) {
                 DispatchQueue.main.async {
                     self.image = cachedImage
                 }
                 return
             }
+            
+            guard let path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else { return }
+            let filePath = URL(fileURLWithPath: path).appendingPathComponent(cachedKey)
+            let fileManager = FileManager()
+            
+            if fileManager.fileExists(atPath: filePath.path) {
+                guard let imageData = try? Data(contentsOf: filePath) else {
+                    print("Disk Cache의 image data가 없습니다.")
+                    return
+                }
+                
+                guard let cachedImage = UIImage(data: imageData) else {
+                    print("Disk Cache의 image data가 없습니다.")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.image = cachedImage
+                    ImageCacheManager.shared.setObject(cachedImage, forKey: cachedKey as NSString)
+                }
+            }
+
             
             guard let url = URL(string: url) else { return }
             
@@ -33,7 +55,7 @@ extension UIImageView {
                 
                 DispatchQueue.main.async {
                     if let data = data, let image = UIImage(data: data) {
-                        ImageCacheManager.shared.setObject(image, forKey: cachedKey)
+                        ImageCacheManager.shared.setObject(image, forKey: cachedKey as NSString)
                         self.image = image
                     }
                 }
