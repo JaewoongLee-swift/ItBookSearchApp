66e7a7d438b18bf4363c9d4a0a494f710eff79b8
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-11-12 17:36:17 +0900

    [Feat] #19 Image 데이터 호출로직을 Data 구조체 사용에서 URLSession을 통한 데이터호출로 변경

--- ItBookSearchApp/ItBookSearchApp/UIImageView+.swift
+++ ItBookSearchApp/ItBookSearchApp/UIImageView+.swift
@@ -9,7 +9,9 @@ import UIKit
 
 extension UIImageView {
     //TODO: Unit Test 필요
-    func setImage(url: String) {
+    func setImage(url: String, session: URLSessionProtocol = URLSession.shared) {
+        let session = session
+        
         DispatchQueue.global(qos: .background).async {
             let cachedKey = NSString(string: "url")
             
@@ -20,14 +22,24 @@ extension UIImageView {
             }
             
             guard let url = URL(string: url) else { return }
-            guard let data = try? Data(contentsOf: url) else { return }
             
-            DispatchQueue.main.async {
-                if let image = UIImage(data: data) {
-                    ImageCacheManager.shared.setObject(image, forKey: cachedKey)
-                    self.image = image
+            let dataTask: URLSessionDataTaskProtocol = session.dataTask(with: url) { data, response, error in
+                guard error == nil else {
+                    DispatchQueue.main.async {
+                        self.image = UIImage()
+                    }
+                    
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    if let data = data, let image = UIImage(data: data) {
+                        ImageCacheManager.shared.setObject(image, forKey: cachedKey)
+                        self.image = image
+                    }
                 }
             }
+            dataTask.resume()
         }
     }
 }
