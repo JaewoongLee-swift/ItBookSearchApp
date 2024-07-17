892564d7b9792e997c34f32226b8fb34b7a46cd9
JaewoongLee-swift <jaewoong0624@gmail.com>
2024-07-17 20:29:43 +0900

    [Feat] #39 NetworkAPIDefinition에 프로퍼티로 RequestInfo, URLSessionProtocol 추가 및 extension에 request 메소드 구현

--- ItBookSearchApp/ItBookSearchApp/Network/APIKit/NetworkAPIDefinition.swift
+++ ItBookSearchApp/ItBookSearchApp/Network/APIKit/NetworkAPIDefinition.swift
@@ -8,11 +8,36 @@
 import Foundation
 
 protocol NetworkAPIDefinition {
+    typealias URLInfo = NetworkAPI.URLInfo
+    typealias RequestInfo = NetworkAPI.RequestInfo
+    
     associatedtype Parameter: Encodable
     associatedtype Response: Decodable
     
-    var urlInfo: NetworkAPI.URLInfo { get }
-    var method: NetworkAPI.Method { get }
-    var headers: [String: String]? { get }
-    var parameters: Parameter? { get }
+    var urlInfo: URLInfo { get }
+    var requestInfo: RequestInfo<Parameter> { get }
+    var session: URLSessionProtocol { get }
 }
+
+extension NetworkAPIDefinition {
+    func request(completion: @escaping (Result<Response, Error>) -> Void) -> URLSessionDataTaskProtocol {
+        let url = urlInfo.url
+        let request = requestInfo.request(url: url)
+        
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            guard let data else { return }
+            
+            do {
+                let response = try JSONDecoder().decode(Response.self, from: data)
+                completion(.success(response))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        dataTask.resume()
+        return dataTask
+    }
+}
+
+
