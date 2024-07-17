f9d2c11dfa3fa95ef0fee57fffe07bd6a23aa3d2
JaewoongLee-swift <jaewoong0624@gmail.com>
2024-07-17 20:28:22 +0900

    [Feat] #39 URLSessionProtocol에 파라미터로 URLRequest를 받는 dataTask 메소드 추가 및 URLSession에 구현

--- ItBookSearchApp/ItBookSearchApp/Network/URLSessionProtocol.swift
+++ ItBookSearchApp/ItBookSearchApp/Network/URLSessionProtocol.swift
@@ -9,11 +9,16 @@ import Foundation
 
 protocol URLSessionProtocol {
     func dataTask(with url: URL, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+    func dataTask(with urlRequest: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
 }
 
 //MARK: Conform the protocol
 extension URLSession: URLSessionProtocol {
     func dataTask(with url: URL, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
         dataTask(with: url, completionHandler: completionHandler) as URLSessionDataTask
+    }
+    
+    func dataTask(with urlRequest: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        dataTask(with: urlRequest, completionHandler: completionHandler) as URLSessionDataTask
     }
 }
