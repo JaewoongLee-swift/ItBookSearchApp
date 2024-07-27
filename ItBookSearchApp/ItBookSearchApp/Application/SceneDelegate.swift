dc2027cede3c9d9bac20f96c21ed9e48a99b76d9
JaewoongLee-swift <jaewoong0624@gmail.com>
2024-07-27 20:57:32 +0900

    [Refactor] #39 SearchViewController에서 ItBook.Search를 init에서 주입받고, requestItBookStore 메소드가 ItBookAPI.Search를 사용하도록 변경

--- ItBookSearchApp/ItBookSearchApp/Application/SceneDelegate.swift
+++ ItBookSearchApp/ItBookSearchApp/Application/SceneDelegate.swift
@@ -11,13 +11,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
         window = UIWindow(windowScene: windowScene)
         
-        self.window?.rootViewController = UINavigationController(rootViewController: SearchViewController())
+        let rootViewController = SearchViewController(searchApi: ItBookAPI.Search())
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
     }
 
---
+++ ItBookSearchApp/ItBookSearchAppTests/Mock/MockURLSession/HTTPURLResponse+.swift
@@ -0,0 +1,21 @@
+//
+//  Extension+URLResponse.swift
+//  ItBookSearchAppTests
+//
+//  Created by Jaewoong Lee on 7/27/24.
+//
+
+import Foundation
+
+extension URLResponse {
+    static func searchURLResponse(statusCode: Int) ->  HTTPURLResponse {
+        let url = URL(string: "https://api.itbook.store/1.0/search/")!
+        
+        return .init(
+            url: url,
+            statusCode: statusCode,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+    }
+}
